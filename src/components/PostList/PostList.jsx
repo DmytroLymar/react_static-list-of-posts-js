76909cb766c2e29ae0fac7b483af730f98af9915
Dmytro Lymar <dmytrolymar901@gmail.com>
2025-10-01 15:55:08 +0300

    Fix

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,11 +1,10 @@
 import { PostInfo } from '../PostInfo/PostInfo';
+import './PostList.scss';
 
 export const PostList = ({ posts }) => (
   <div className="PostList">
-    <div className="PostList">
-      {posts.map(post => (
-        <PostInfo post={post} key={post.id} />
-      ))}
-    </div>
+    {posts.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
   </div>
 );
